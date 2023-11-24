3f26da106c069e0f58fa16267f49e1d8cb2c2e22
Frans <fjaspers@xs4all.nl>
2023-11-24 11:43:13 +0100

    Removed obsolete files

--- src/hooks/use-resv-context.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import ResvCtx, { type IResvCtx } from "../context/Resv";
-
-function useResvContext() {
-  return useContext<IResvCtx>(ResvCtx);
-}
-
-export default useResvContext;
